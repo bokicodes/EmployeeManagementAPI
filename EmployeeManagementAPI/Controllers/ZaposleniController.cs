01c05e0a16556a569fca21866cca32c5aa6849e1
bokicodes <radmilacb@gmail.com>
2025-01-15 17:37:42 +0100

    dodao SeriLog i isprobao

--- EmployeeManagementAPI/Controllers/ZaposleniController.cs
+++ EmployeeManagementAPI/Controllers/ZaposleniController.cs
@@ -10,15 +10,19 @@ namespace EmployeeManagementAPI.Controllers;
 public class ZaposleniController : ControllerBase
 {
     private readonly IZaposleniRepository _zaposleniRepo;
+    private readonly ILogger<ZaposleniController> _logger;
 
-    public ZaposleniController(IZaposleniRepository zaposleniRepo)
+    public ZaposleniController(IZaposleniRepository zaposleniRepo, ILogger<ZaposleniController> logger)
     {
         _zaposleniRepo = zaposleniRepo;
+        _logger = logger;
     }
 
     [HttpGet]
     public async Task<IEnumerable<Zaposleni>> GetAll()
     {
+        _logger.LogInformation("Poziva se metoda za vracanje svih zaposlenih...");
+
         return await _zaposleniRepo.GetAllAsync();
     }
 }
--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -2,10 +2,20 @@ using EmployeeManagementAPI.Data;
 using EmployeeManagementAPI.Data.Interfaces;
 using EmployeeManagementAPI.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
